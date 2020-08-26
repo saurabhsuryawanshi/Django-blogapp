681890e55ab73389d9f52e4bd77693655b9866f9
saurabh suryawanshi <saurabhsuryawanshi991@gmail.com>
2020-08-26 14:34:32 +0530

    good project

--- blogapp/blogapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render , redirect
-from django.conf import settings
-
-User=settings.AUTH_USER_MODEL
-
-def home_page(request):
-	return render(request,"index.html",{})
-
